ceadf46b3403cc011892aa4e41fe95adeec6504a
vineetshr <vineet.shrimanker.21@ucl.ac.uk>
2024-05-20 14:16:34 +0100

    GameForm JPanel created

--- src/GameForm.java
+++ src/GameForm.java
@@ -1,3 +1,22 @@
-public class GameForm {
+import javax.swing.JFrame;
 
+public class GameForm extends JFrame {
+    public GameForm()  {
+
+        this.add(new GameArea());
+
+    }
+
+    public static void main(String[] args) {
+
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new GameForm().setVisible(true);
+
+            }
+        });
+
+    }
 }
